be70564407575013041dc018704ce025e5daa4e0
Yat Long Chan <yatlongc2005@gmail.com>
2022-12-16 18:07:58 -0500

    list of tuples for watchmode

--- app/apis/watchmode.py
+++ app/apis/watchmode.py
@@ -1,6 +1,6 @@
 import requests
 
-KEY = open('keys/key_watchmode.txt', 'r').read().strip()
+KEY = open('../keys/key_watchmode.txt', 'r').read().strip()
 SITES = ['Netflix', 'Amazon', 'Hulu', 'HBO Max', 'Disney+', 'iTunes']
 # returns info about streaming services in dict
 def get_streaming(imdb_id):
@@ -8,10 +8,13 @@ def get_streaming(imdb_id):
   streaming = requests.get(url).json()
   ret = []
   for d in streaming:
-    if d['name'] in SITES:
-      ret.append( (d['name'], d['web_url']) )
+    tupl = (d['name'], d['web_url'])
+    if d['name'] in SITES and tupl not in ret:
+      ret.append(tupl)
   return ret
 
 def get_trailer(imdb_id):
   url = f'https://api.watchmode.com/v1/title/{imdb_id}/details/?apiKey={KEY}'
   return requests.get(url).json().get('trailer')
+
+# print(get_streaming('tt0120338'))
